95951555b05869ce7a09e0901215d8d316d15cac
jbachurski <kbachurski@gmail.com>
2019-05-07 19:02:39 +0200

    Add checker & HTML doc support, minor Linux compatibility fixed

--- iotapack.py
+++ iotapack.py
@@ -14,7 +14,7 @@ def trimcwd(p):
 def copy_file(src, dest):
     print(f"{trimcwd(src)} --> {trimcwd(dest)}")
     shutil.copy(str(src), str(dest))
-    
+
 def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
@@ -27,13 +27,29 @@ def convert_crlf_to_lf(file):
     with file.open(mode="wb") as f:
         f.write(data)
 
-def main(name, model, lang, inputs, doc, cfg, add_sol, pdf_src=None, do_zip=False):
+def get_ext(filename):
+    return os.path.splitext(filename)[1][1:]
+
+def main(name, model, lang, inputs, doc, cfg, add_sol, pdf_src=None, checker=None, do_zip=False):
     start = time.time()
-    
+    cleanup = []
+
     model = Path.cwd() / Path(model)
     inputs = Path.cwd() / Path(inputs)
+
+    force_doc_ext = None
+    if get_ext(doc) == "html":
+        _doc = doc
+        doc = f"{name}doc.tmp.html.zip"
+        with zipfile.ZipFile(doc, mode='w') as doczip:
+            doczip.write(_doc, arcname="index.html")
+        cleanup.append(doc)
+        force_doc_ext = "html.zip"
+    elif get_ext(doc) == "zip":
+        force_doc_ext = "html.zip"
+
     doc = Path.cwd() / Path(doc)
-    
+
     assert lang in ("py", "cpp")
 
     if lang == "py":
@@ -42,13 +58,13 @@ def main(name, model, lang, inputs, doc, cfg, add_sol, pdf_src=None, do_zip=Fals
         model_caller = str(model)[:-4] + (".exe" if sys.platform == "win32" else "")
 
     base = Path.cwd() / name
-    
+
     shutil.rmtree(base, ignore_errors=True)
     while os.path.exists(str(base)):
         pass
 
     (base / "doc").mkdir(parents=True, exist_ok=True)
-    copy_file(doc, base / "doc" / f"{name}zad.pdf")
+    copy_file(doc, base / "doc" / f"{name}zad.{force_doc_ext if force_doc_ext else get_ext(doc)}")
 
     (base / "prog").mkdir(parents=True, exist_ok=True)
     copy_file(model, base / "prog" / f"{name}.{lang}")
@@ -56,7 +72,7 @@ def main(name, model, lang, inputs, doc, cfg, add_sol, pdf_src=None, do_zip=Fals
     (base / "in").mkdir(parents=True, exist_ok=True)
     (base / "out").mkdir(parents=True, exist_ok=True)
     print("Looking for inputs", str(inputs / "*.in"))
-    for file in glob.glob(str(inputs / "*.in")):
+    for file in sorted(glob.glob(str(inputs / "*.in"))):
         file = Path(file)
 
         infile  = (base / "in"  / (f"{name}" + file.name))
@@ -65,32 +81,43 @@ def main(name, model, lang, inputs, doc, cfg, add_sol, pdf_src=None, do_zip=Fals
         copy_file(file, infile)
 
         print(f"$ {trimcwd(model_caller)} < {trimcwd(infile)} > {trimcwd(outfile)}")
-        os.system(f"{model_caller} < {infile} > {outfile}")
+        os.system(f"'{model_caller}' < '{infile}' > '{outfile}'")
 
         if sys.platform == "win32":
             convert_crlf_to_lf(infile)
             convert_crlf_to_lf(outfile)
-    
+
     if cfg is not None:
         copy_file(Path(cfg), base / "config.yml")
-    
+
     if add_sol is not None:
         for sol_f in add_sol:
             sol_f = Path(sol_f)
             copy_file(sol_f, base / "prog" / sol_f.name)
-    
+
     if pdf_src is not None:
         pdf_src = Path.cwd() / Path(pdf_src)
         copy_file(pdf_src, base / "doc" / pdf_src.name)
+
+    if checker is not None:
+        checker = Path.cwd() / Path(checker)
+        copy_file(checker, base / "prog" / f"{name}chk.{get_ext(str(checker))}")
 
     if do_zip:
         print("zip... ", end="", flush=True)
         zipdir(f"{name}", zipfile.ZipFile(f"{name}.zip", "w", zipfile.ZIP_DEFLATED))
         print("done")
-    
+
     finish = time.time()
+
+    if cleanup:
+        print("Cleaning up...")
+        for file in cleanup:
+            print(f"## {file}")
+            os.remove(file)
+
     print(f"[!] done in: {round(finish - start, 3)}s")
-        
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="iotapack")
     parser.add_argument("name", help="The name for the problem (the ID that is used in SIO2).")
@@ -100,8 +127,9 @@ if __name__ == "__main__":
     parser.add_argument("doc", help="Problem statement file.")
     parser.add_argument("-c", "--cfg", default=None, help="The .yml config file")
     parser.add_argument("-a", "--addsol", action="append", help="Specify an additional solution (can be used multiple times)")
-    parser.add_argument("-ps", "--pdfsrc", default=None, help="Embed the pdf source (probably a .tex file)")
+    parser.add_argument("-ps", "--pdfsrc", default=None, help="Embed the text source (probably a .tex file)")
+    parser.add_argument("-k", "--checker", default=None, help="Checker program, for checking the correctness of results")
     parser.add_argument("-z", "--zip", action="store_true", help="Flag that creates a zip file immediately after packing is done")
-    
+
     args = parser.parse_args()
-    main(args.name, args.model, args.lang, args.inputs, args.doc, args.cfg, args.addsol, args.pdfsrc, args.zip)
+    main(args.name, args.model, args.lang, args.inputs, args.doc, args.cfg, args.addsol, args.pdfsrc, args.checker, args.zip)
